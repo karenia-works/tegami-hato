c9ad427b33d0155b96bed5c0d65a429f2486dc6d
“MaksimXing <henryx@buaa.edu.cn>
2020-01-10 18:49:07 +0800

    basic init and component

--- web/tegami-hato/src/app/app-routing.module.ts
+++ web/tegami-hato/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MainPageComponent } from "src/pages/main-page/main-page.component";
+import { MainPageModule } from "src/pages/main-page/main-page.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: MainPageComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), MainPageModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
