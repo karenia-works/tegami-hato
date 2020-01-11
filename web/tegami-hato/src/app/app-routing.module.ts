832284667f641622f7b53ad4046a3bac13d453bb
“MaksimXing <henryx@buaa.edu.cn>
2020-01-11 15:56:58 +0800

    group-page init

--- web/tegami-hato/src/app/app-routing.module.ts
+++ web/tegami-hato/src/app/app-routing.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MainPageComponent } from "src/pages/main-page/main-page.component";
 import { MainPageModule } from "src/pages/main-page/main-page.module";
+import { GroupPageComponent } from "src/pages/group-page/group-page.component";
+import { GroupPageModule } from "src/pages/group-page/group-page.module";
 
 const routes: Routes = [
   {
     path: "",
     component: MainPageComponent
-  }
+  },
+  {
+    path: "g",
+    component: GroupPageComponent
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), MainPageModule],
+  imports: [RouterModule.forRoot(routes), MainPageModule,GroupPageModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
---
+++ web/tegami-hato/src/pages/group-page/group-page.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GroupPageComponent } from './group-page.component';
+
+
+
+@NgModule({
+  declarations: [GroupPageComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [GroupPageComponent]
+})
+export class GroupPageModule { }
