a1cf82b74bcc937743b36ef4a859fcb6902953ab
“MaksimXing <henryx@buaa.edu.cn>
2020-02-19 18:38:48 +0800

    homepage done

--- web/tegami-hato/src/sample/followList.ts
+++ web/tegami-hato/src/sample/followList.ts
@@ -1,17 +1,38 @@
 export const followList = [
   {
-    name: '1721大班群'
+    name: "1721大班群"
   },
   {
-    name: '1737大班群'
+    name: "1737大班群"
   },
   {
-    name: 'GUGUGU小组'
+    name: "GUGUGU小组"
   },
   {
-    name: '北京航空航天大学'
+    name: "北京航空航天大学"
   },
   {
-    name: '士谔书院'
+    name: "士谔书院"
+  }
+];
+
+export const NoticeList = [
+  {
+    title: "关于防疫的指示",
+    from: "校团委",
+    time: "2012-10-5 8:13",
+    content:"此命令用来放弃掉所有还没有加入到缓存区（就是 git add 命令）的修改：内容修改与整个文件删除命令不会删除新建的文件，因为新建的文件还没加入git管理系统中，所以对git来说是未知，只需手动删除即可"
   },
+  {
+    title: "有一说一，你是真的牛啤",
+    from: "我猜我是谁",
+    time: "1332-12-5 8:13",
+    content:" it worked with that, no need fro BrowserModule in my case import { CommonModule } from '@angular/common'  "
+  },
+  {
+    title: "咕了咕了，告辞",
+    from: "GUGUGUGUGU",
+    time: "4132-3-5 8:13",
+    content: "你输入的 true和false是字符串，angular判定字符串不为空就对的，user.virtual = true，所以不会消失。你后台个给user.virtual初始化数据了？"
+  }
 ];
